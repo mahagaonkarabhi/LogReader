dd17b34666952978f87aaf99f95169da38898738
mahagaonkarabhi <105964615+mahagaonkarabhi@users.noreply.github.com>
2022-05-21 16:14:10 +0530

    Update LogDetails.java

--- src/main/java/com/cs/assesment/model/LogDetails.java
+++ src/main/java/com/cs/assesment/model/LogDetails.java
@@ -1,1 +1,83 @@
+package com.cs.assesment.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.internal.NotNull;
+
+
+public class LogDetails {
+
+    @NotNull
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("state")
+    private String state;
+
+    @JsonProperty("timestamp")
+    private Long timeStamp;
+
+    @JsonProperty("type")
+    private String type;
+
+    @JsonProperty("host")
+    private String host;
+
+    private long duration;
+    private boolean alert;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public boolean isAlert() {
+        return alert;
+    }
+
+    public void setAlert(boolean alert) {
+        this.alert = alert;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+}
