abe350164274b1eac6de3d5a588406f6a85e8e45
mahagaonkarabhi <105964615+mahagaonkarabhi@users.noreply.github.com>
2022-05-21 16:12:36 +0530

    Update DataMapper.java

--- src/main/java/com/cs/assesment/mapper/DataMapper.java
+++ src/main/java/com/cs/assesment/mapper/DataMapper.java
@@ -1,1 +1,20 @@
+package com.cs.assesment.mapper;
 
+import com.cs.assesment.model.LogDetails;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+public class DataMapper {
+    private static final Logger logger = Logger.getLogger(DataMapper.class.getName());
+
+    public LogDetails getLogDetails(ObjectMapper objectMapper, String content) {
+        try {
+            return objectMapper.readValue(content, LogDetails.class);
+        } catch (IOException e) {
+            logger.debug("Exception occurred while parsing the input file ", e);
+        }
+        return null;
+    }
+}
